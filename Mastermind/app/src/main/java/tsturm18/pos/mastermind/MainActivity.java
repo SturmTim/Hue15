8512a7e1efbea17b68045fff5b67e12ff4515e70
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2021-02-01 13:45:11 +0100

    create Project

---
+++ Mastermind/app/src/main/java/tsturm18/pos/mastermind/MainActivity.java
@@ -0,0 +1,14 @@
+package tsturm18.pos.mastermind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
